06370791fa117c3dab6e85e18ebc5c784a9c0e05
Nik <your@mail.com>
2025-09-15 22:37:53 +0200

    Fix solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import requests
 
 URL = "https://api.weatherapi.com/v1/current.json?"
@@ -8,14 +9,20 @@ API_KEY = os.environ.get("API_KEY")
 
 
 def get_weather() -> None:
+
+    if not API_KEY:
+        print("Error: API_KEY environment variable not set!")
+        sys.exit(1)
     response = requests.get(URL + f"key={API_KEY}&q={FILTER}")
     if response.status_code == 200:
         data = response.json()
+        location = f"{data['location']['name']}/{data['location']['country']}"
+        localtime = data['location']['localtime']
+        temp = f"{data['current']['temp_c']}°C"
+        condition = data['current']['condition']['text']
 
-        print(f"{data["location"]["name"]}/{data["location"]["country"]} "
-              f"{data["location"]["localtime"]} "
-              f"Weather: {data["current"]["temp_c"]} Celsius, "
-              f"{data["current"]["condition"]["text"]}")
+        print(f"{location} {localtime} Weather: {temp}, {condition}")
+
     else:
         print("Error:", response.status_code, response.text)
 
