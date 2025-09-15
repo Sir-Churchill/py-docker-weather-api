31696d775311936df41560f470dedbf37b91ed82
Nik <your@mail.com>
2025-09-15 22:06:10 +0200

    Fix solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import os
-
 import requests
+
 URL = "https://api.weatherapi.com/v1/current.json?"
 FILTER = "Paris"
 
@@ -9,13 +9,15 @@ API_KEY = os.environ.get("API_KEY")
 
 def get_weather() -> None:
     response = requests.get(URL + f"key={API_KEY}&q={FILTER}")
+    if response.status_code == 200:
+        data = response.json()
 
-    data = response.json()
-
-    print(f"{data["location"]["name"]}/{data["location"]["country"]} "
-          f"{data["location"]["localtime"]} "
-          f"Weather: {data["current"]["temp_c"]} Celsius, "
-          f"{data["current"]["condition"]["text"]}")
+        print(f"{data["location"]["name"]}/{data["location"]["country"]} "
+              f"{data["location"]["localtime"]} "
+              f"Weather: {data["current"]["temp_c"]} Celsius, "
+              f"{data["current"]["condition"]["text"]}")
+    else:
+        print("Error:", response.status_code, response.text)
 
 
 if __name__ == "__main__":
